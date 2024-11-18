39959e2cf3f866ac9cd3508d402a4f3d15ff8357
VitaFrancesco <vitafrancesco00@gmail.com>
2024-11-18 13:16:11 +0100

    ADD: array of object with post

---
+++ post.js
@@ -0,0 +1,34 @@
+post = [
+    {
+        title: 'ciambellone',
+        content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+        image: '/images/ciambellone.jpeg',
+        tags: ['ciambellone', 'sweet', 'donut']
+    },
+    {
+        title: 'cracker di barbabietola',
+        content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo, laborum',
+        image: '/images/cracker_barbabietola.jpeg',
+        tags: ['cracker', 'barbabietola', 'snack']
+    },
+    {
+        title: 'pane fritto dolce',
+        content: 'Lorem, ipsum dolor sit amet consectetur',
+        image: '/images/pane_fritto_dolce.jpeg',
+        tags: ['pane', 'fritto', 'dolce']
+    },
+    {
+        title: 'pasta con barbabietola',
+        content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo, laborum',
+        image: '/images/pasta_barbabietola.jpeg',
+        tags: ['pasta', 'barbabietola', 'lunch']
+    },
+    {
+        title: 'torta paesana',
+        content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo',
+        image: '/images/torta_paesana.jpeg',
+        tags: ['torta_paesana', 'sweet']
+    }
+];
+
+module.exports = post;
