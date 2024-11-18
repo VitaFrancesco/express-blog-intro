35308c2f32c121ffe4dd8ae29123b9cd958891ab
VitaFrancesco <vitafrancesco00@gmail.com>
2024-11-18 13:23:30 +0100

    ADD: route with  /bacheca with array post and static asset

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const post = require('./post.js');
 
 app.get('/', (req, res) => {
     res.send(`<h1>Server del mio blog</h1>`)
+});
+
+app.use(express.static('public'));
+
+app.get('/bacheca', (req, res) => {
+    res.json(post);
 });
 
 app.listen(port, () => {
